a6e653f66b7cf27b23a2d647e7809c04b3ceb84e
LuzI-Gs <luz22955@gmail.com>
2021-09-15 17:41:47 -0500

    Se agrego codigo

--- ProyectosJavaPractica/PracticaJava/src/App.java
+++ ProyectosJavaPractica/PracticaJava/src/App.java
@@ -1,6 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
        // System.out.println("Hello, World!");
-        System.out.println("Hola WALTER WHITE ALIAS HEISENWERG");
+        //System.out.println("Hola WALTER WHITE ALIAS HEISENWERG");
+
+        int Valor1=0;
+        int Valor2=1;
+        int ValorDeCiclo=10;
+        //int Suma=Valor1+Valor2;
+        int Suma;
+        System.out.println("EL valor 1 es : " +Valor1);
+        System.out.println("El valor 2 es : "+Valor2);
+         for(int i=0;i<ValorDeCiclo;i++)
+         {
+            
+            Suma=Valor1+Valor2;
+            System.out.println(Suma);
+            Valor1=Valor2;
+            Valor2=Suma;
+
+         }
     }
 }
